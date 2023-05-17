e7467886c2a30ac328fa2e1163aaf0655d02a53f
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-05-17 13:57:40 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list = document.querySelector('#ingredients');
+let inner;
+ingredients.forEach(ingredient => {
+  const li = document.createElement('li');
+  li.classList.add('item');
+  console.log(li);
+   li.textContent = ingredient;
+list.append(li)
+
+});
+
+console.log(list);
